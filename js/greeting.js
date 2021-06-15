622c156af7e737279967eb95772f7de28a03ef30
Cristian Eduardo Feldman <42149568+Herzogs@users.noreply.github.com>
2021-06-15 18:35:49 -0300

    Update greeting

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Cristian Feldman';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
