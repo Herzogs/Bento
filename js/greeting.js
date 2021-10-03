cf1af073a68f097587b28ff0cca8c7674ea203e8
Cristian Eduardo Feldman <42149568+Herzogs@users.noreply.github.com>
2021-10-03 10:52:04 -0300

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -9,17 +9,12 @@ const name = ' Cristian Feldman';
 const gree1 = 'Go to Sleep!  ';
 const gree2 = 'Good morning!  ';
 const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 22 && hour < 6) {
   document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
-} else if (hour >= 12 && hour < 17) {
+} else if (hour >= 12 && hour < 22) {
   document.getElementById('greetings').innerText = gree3 + name;
-} else {
-  document.getElementById('greetings').innerText = gree4 + name;
 }
